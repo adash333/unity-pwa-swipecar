4a7a8ee76b042ac073c179b81e35f414594ef237
adash333 <adash333@hotmail.com>
2019-08-23 19:00:55 +0900

    first commit

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+	GameObject car; 
+
+	// Use this for initialization
+	void Start () {
+		this.car = GameObject.Find ("car");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick() {
+		Debug.Log ("Clicked");
+
+		// transformを取得
+		Transform myTransform = this.car.transform;
+
+		// World座標を基準に、座標を取得
+		Vector2 worldPos = myTransform.position;
+		worldPos.x = -2.8f;
+		worldPos.y = -2.9f;
+		myTransform.position = worldPos;
+	}
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI部品を使用するために必要
+
+public class GameDirector : MonoBehaviour {
+
+	GameObject car; 
+	GameObject flag;
+	GameObject distance;
+
+	// Use this for initialization
+	void Start () {
+		this.car = GameObject.Find ("car"); // このように記載することでプロジェクト内のcarオブジェクトを見つけることができる
+		this.flag = GameObject.Find("flag");
+		this.distance = GameObject.Find("Distance");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float length = this.flag.transform.position.x - this.car.transform.position.x;
+
+		if (length >= 0) {
+			this.distance.GetComponent<Text> ().text = "ゴールまで" + length.ToString ("F2") + "m";
+		} else {
+			this.distance.GetComponent<Text> ().text = "Game Over";
+		}
+
+	}
+
+}
